3dcc62bb446c13197c0047bfc34aa1ba41712514
Jaime <quezadajaime@ymail.com>
2023-07-21 19:48:13 -0700

    added various arrays

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,8 +2,14 @@
 document.querySelector("#generate").addEventListener(
   "click", writePassword); 
 
+  //adding various arrays
   var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialChar = [  ]
+  var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+  var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+  
 
 
 
