19c433b5e3b5f446d504cb6c4d18a1bbad79404c
Jaime <quezadajaime@ymail.com>
2023-07-23 06:26:01 -0700

    adding completed code

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Password-Generator
+
+
+
+
+
+
+
+## Description
+
+This webpage allows the user to create a randomized password. The user is prompted when "Generate Password" button is clicked. A password is generated based on the users inputs.
+
+![Password-Test](https://github.com/quezadajaime/Password-Generator/assets/136545874/a462b408-bd9e-4042-bfe8-ea9120980170)
+
+If the criteria for the password are not met the users will be prompted with the following errors:
+
+![Password-Error](https://github.com/quezadajaime/Password-Generator/assets/136545874/0ed6e1cd-f19d-4705-9e18-cab19f57b9e9)
+
+
+
+
+## Installation
+
+Follow the following link for password generator
+
+https://github.com/quezadajaime/Password-Generator.git
+
+
+
+## Usage
+
+To create a randomized password based on the users inptus. 
+
+## Credits
+
+MDN Web Docs
+
+edX Bootcamp
+
+gif to README upload credit to https://www.youtube.com/watch?v=EF6fqnnl3Uk
+
+## License
+
+MIT License
--- script.js
+++ script.js
@@ -9,46 +9,98 @@ document.querySelector("#generate").addEventListener(
   var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-  function generatePassword() { {
-    var length = parseInt 
+  function createPassword() {
+    var getPasswordLength = parseInt 
       (prompt ("How many charcters would you like your password to contain?"));
-    };
-    
-    if (length<8) {
-      alert("Password must be at leaset 8 characters long");
-      return null;
+      if (getPasswordLength < 8) {
+      alert("Password must be at leaset 8 characters long.");
+      generatePassword();
     }
 
-    if (length>128) {
-      alert("Password length cannot be longer than 128 characters");
-      return null;
+    if (getPasswordLength > 128) {
+      alert("Password length cannot be longer than 128 characters.");
+      generatePassword();
     }
-  
-    var includeAlphaLower = confirm(
+
+    if (isNaN(getPasswordLength)) {
+      alert("Invalid input. Please add a number.");
+      generatePassword();
+    }
+  //Confirming to include lowercase letters
+    var includeAlphaLower = window.confirm(
       "Click OK to confirm including lower case characters"
     );
-
-    var includeAlphaUpper = confirm(
+//confirming to include upper case
+    var includeAlphaUpper = window.confirm(
       "Click OK to confirm including upper case characters"
     );
-
-    var includeSpecialChar = confirm(
+//confiring special characters
+    var includeSpecialChar = window.confirm(
       "Click OK to confirm including special characters"
     );
-
-    var includeNumber = confirm(
+//confirming numbers
+    var includeNumber = window.confirm(
       "Click OK to confirm including number characters"
     );
 
     if (!includeAlphaLower && !includeAlphaUpper && !includeSpecialChar && !includeNumber){
     alert ("Password must include at least one character type.");
+    return;
     }
+//stores password info in object
+    var passwordInfo = {
+      passwordLength: getPasswordLength,
+      includeAlphaLower: includeAlphaLower,
+      includeAlphaUpper: includeAlphaUpper,
+      includeNumber: includeNumber,
+      includeSpecialChar: includeSpecialChar,
+    };
+
+    return passwordInfo;
+  }
+// get a random character from an array
+  function getRandomChar(arr) {
+    var randomChar = Math.floor(Math.random() * arr.length);
+    var selectedChar = arr[randomChar];
+
+    return selectedChar;
   }
 
+  function generatePassword() {
+    //passwordInfo object
+    var passwordIncludes = createPassword();
+//store create
+    var createdPassword = [];
 
+    var availableCharacters = [];
+// adds array of lower case characters to avaialable characters
+    if(passwordIncludes.includeAlphaLower) {
+      availableCharacters = availableCharacters.concat(alphaLower);
+    }
+// adds array of upper case to available characters
+    if(passwordIncludes.includeAlphaUpper) {
+      availableCharacters = availableCharacters.concat(alphaUpper);
+    }
+//adds array of special characters to available characters
+    if(passwordIncludes.includeSpecialChar){
+      availableCharacters = availableCharacters.concat(specialChar);
+    }
+//adds array of numbers to avaialbe characters
+    if(passwordIncludes.includeNumber){
+      availableCharacters = availableCharacters.concat(number);
+    }
+//Itarate through password length, get random index from the array of available characters
+    for (var i =0; i < passwordIncludes.passwordLength; i++) {
+      var c = getRandomChar(availableCharacters);
 
-  
+// add characters to password array
+      createdPassword.push(c);
+    }
 
+      return createdPassword.join("");
+    
+
+  }
 
 
 // Get references to the #generate element
